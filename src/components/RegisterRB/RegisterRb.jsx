b45ecb77b0ee8c9ed2009afa6ff44397e60cb5e4
Md-Azad <sokalazad@gmail.com>
2023-04-17 19:34:47 +0200

    adde password validation with regex

--- src/components/RegisterRB/RegisterRb.jsx
+++ src/components/RegisterRB/RegisterRb.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from "../../Firebase/firebase.config";
 
 const RegisterRb = () => {
+  const [registerError, setRegisterError] = useState("");
+  const [valid, setValid] = useState("");
+
   const auth = getAuth(app);
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    setRegisterError("");
+    setValid("");
     const email = event.target.email.value;
     const password = event.target.password.value;
+    if (!/(?=.*\d)/.test(password)) {
+      setValid("Password should contain at least one digit");
+      return;
+    } else if (!/(?=.*[A-Z])/.test(password)) {
+      setValid("Password should contain at least one UpperCase");
+      return;
+    } else if (!/(?=.*[!#$%&? "])/.test(password)) {
+      setValid("Password should contain at least one speceial character");
+      return;
+    }
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const user = result.user;
         console.log(user);
       })
       .catch((error) => {
+        setRegisterError(error.message);
         console.error(error.message);
       });
   };
@@ -24,7 +40,12 @@ const RegisterRb = () => {
       <Form onSubmit={handleFormSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" name="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -33,6 +54,7 @@ const RegisterRb = () => {
             type="password"
             name="password"
             placeholder="Password"
+            required
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -42,6 +64,8 @@ const RegisterRb = () => {
           Submit
         </Button>
       </Form>
+      <p>{registerError}</p>
+      <p className="text-danger">{valid}</p>
     </div>
   );
 };
